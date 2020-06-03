d601c75e57b9b9c969e98fe19144f2caab44af1e
Dan Sensecall <dan@sensecall.co.uk>
2020-06-03 13:11:57 +0100

    journeys

--- app/routes.js
+++ app/routes.js
@@ -5,9 +5,16 @@ const router = express.Router()
 
 module.exports = router
 
-router.use(/\/([0-9]+)-([0-9]+)/, (req, res, next) => {
+router.use(/\/delete-([0-9]+)-([0-9]+)/, (req, res, next) => {
 	req.version = req.originalUrl.split('/')[1]
 	req.session.data['prototypeVersion'] = req.version
 	
-	require(`./views/${req.params[0]}-${req.params[1]}/routes`)(req, res, next);
+	require(`./views/delete-${req.params[0]}-${req.params[1]}/routes`)(req, res, next);
+})
+
+router.use(/\/statuses-([0-9]+)-([0-9]+)/, (req, res, next) => {
+	req.version = req.originalUrl.split('/')[1]
+	req.session.data['prototypeVersion'] = req.version
+	
+	require(`./views/statuses-${req.params[0]}-${req.params[1]}/routes`)(req, res, next);
 })
---
+++ app/views/statuses-1-0/routes.js
@@ -0,0 +1,76 @@
+const express = require('express')
+const router = express.Router()
+
+// Add your routes here - above the module.exports line
+
+module.exports = router
+
+router.get('/', (req, res, next) => {
+	res.redirect(`/${req.version}/config`)
+})
+
+function createBusinessList(l, i) {
+	let x = Array.from({length: i}, () => Math.floor(Math.random() * l)).sort((a, b) => a - b)
+	return x
+}
+
+router.post('/config', (req, res, next) => {
+	req.session.data['random-businesses'] = createBusinessList(req.session.data['businesses'].length, req.session.data['orgs-count'])
+	res.redirect('dashboard')
+})
+
+router.get('/organisations', (req, res, next) => {
+	if(! req.session.data['random-businesses']){
+		req.session.data['random-businesses'] = createBusinessList(req.session.data['businesses'].length, req.session.data['orgs-count'])
+	}
+
+	res.render(`${req.version}/organisations`)
+})
+
+router.post('/search', (req, res, next) => {
+	res.redirect('search-results')
+})
+
+router.post('/confirm-delete', (req, res, next) => {
+	if(req.session.data['confirm-delete'] == 'yes'){
+		delete req.session.data['delete-certificate']
+		res.redirect('delete-details')
+	} else if(req.session.data['confirm-delete'] == 'no'){
+		res.redirect('certificate')
+	} else {
+		res.redirect('confirm-delete--errors')
+	}
+})
+
+router.post('/delete-details', (req, res, next) => {
+	let ref = req.session.data['delete-certificate']['ticket-reference']
+	let reason = req.session.data['delete-certificate']['reason']
+
+	if(ref.length && reason.length){
+		res.redirect('check-answers')
+	} else {
+		req.session.data['delete-certificate']['error-list'] = []
+		let errorList = req.session.data['delete-certificate']['error-list']
+
+		if(!reason.length){
+			errorList.push({
+				text: "Enter a reason for deleting this certificate",
+				href: "#reason"
+			})
+		}
+
+		if(!ref.length){
+			errorList.push({
+				text: "Enter a ticket or incident number",
+				href: "#ticket-reference"
+			})
+		}
+
+		res.redirect('delete-details--errors')
+	}
+})
+
+router.post('/check-answers', (req, res, next) => {
+	delete req.session.data['delete-certificate']
+	res.redirect('delete-success')
+})
